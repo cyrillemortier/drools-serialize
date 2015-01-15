1651e24d197b32db665d74846f1ba19fe65ac3cd
cyrillemortier <cyrille.mortier@gmail.com>
2015-01-15 17:06:56 +0100

    lombok "guide", then clean up test rules

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ serialize a drools rule to be used on android
 
 TO get started : 
 -> eclipse with 
-  --> AVD and api 19
+  --> AVD (api 19)
   --> m2e eclipse
+  --> lombok project (http://projectlombok.org/)
 
 import the project in eclipse,
 you should be good to go after a maven update
--- drools-maven-android/src/test/java/org/hello/createRule.java
+++ drools-maven-android/src/test/java/org/hello/createRule.java
@@ -4,115 +4,62 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.drools.core.util.DroolsStreamUtils;
 import org.junit.Test;
 import org.kie.api.KieBase;
+import org.kie.api.KieBaseConfiguration;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
 import org.kie.api.builder.Results;
+import org.kie.api.conf.EventProcessingOption;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.internal.io.ResourceFactory;
 
+@Slf4j
 public class createRule {
 
 	@Test
 	public void doIt() throws Exception {
-		// File rule = new File(getClass().getClassLoader()
-		// .getResource("helloworld.drl").getPath());
+		 KieServices ks = KieServices.Factory.get();
+		    KieFileSystem kfs = ks.newKieFileSystem();
+		    
+		    
+		    File rule = new File(getClass().getClassLoader()
+					.getResource("HelloWorld.drl").getPath());
+		    kfs.write( "src/main/resources/HelloWorld.drl",
+		                   ks.getResources().newInputStreamResource( rule.toURI().toURL().openStream() ) );
+		    
+		    KieBuilder kieBuilder = ks.newKieBuilder( kfs ).buildAll();
+		    Results results = kieBuilder.getResults();
+		    
+		    if( results.hasMessages( Message.Level.ERROR ) ){
+				for (Message message : results.getMessages()) {
+					log.error( message.getText() );
+				}
+		        throw new IllegalStateException( "### errors ###" );
+		    }
+		    
+		    KieContainer kieContainer =
+		        ks.newKieContainer( ks.getRepository().getDefaultReleaseId() );
 
-		// KieBase newKieBase;
-		// newKieBase = (KieBase) DroolsStreamUtils.streamIn(rules, this
-		// .getClass().getClassLoader(), true);
-		// kieSession = newKieBase.newKieSession();
+		    // CEP - get the KIE related configuration container and set the EventProcessing (from default cloud) to Stream
+		    KieBaseConfiguration config = ks.newKieBaseConfiguration();
+		    config.setOption( EventProcessingOption.STREAM );
+		    KieBase kieBase = kieContainer.newKieBase( config );
+		    File compiledRuleFile = new File(getClass().getClassLoader()
+					.getResource("compiledRules").getPath());
 
-		// InputStream in = new FileInputStream(rule);
-		//
-		// final KieBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
-		// List<KnowledgePackage> pkgs = new ArrayList<KnowledgePackage>();
-		// KnowledgePackage knowledgePackage = null;
-
-		// Object streamIn = DroolsStreamUtils.streamIn(in);
-		// knowledgePackage = (KnowledgePackage) streamIn;
-		// pkgs.add(knowledgePackage);
-		// kbase.(pkgs);
-		// for(KnowledgePackage pkg : kbase.getKnowledgePackages()) {
-		// System.out.println("Loaded rule package: " + pkg.toString());
-		// for (Rule rule : pkg.getRules()) {
-		// System.out.println("Rule: " + rule);
-		// }
-		// }
-		// }
-		//
-
-		// read second rule from String
-		String myRule = " package org.hello"
-				+ " import org.hello.object.Message;"
-				+ " global java.util.List list"
-				+ " rule \"Hello World\""
-				+ "    dialect \"mvel\""
-				+ "     when"
-				+ "         m : Message( status == Message.HELLO, message : message )"
-				+ "     then"
-				+ "     modify ( m ) { message = \"Goodbye cruel world\","
-				+ "                    status = Message.GOODBYE };" + " end";
-
-		KieServices kieServices = KieServices.Factory.get();
-		File rule = new File(getClass().getClassLoader()
-				.getResource("HelloWorld.drl").getPath());
-
-		KieFileSystem kfs = kieServices.newKieFileSystem();
-
-		//
-		// ResourceFactory.newFileResource(rule)
-		//
-		// Resource myResource = kieServices.getResources()
-		// .newReaderResource(new StringReader(myRule));
-		kfs.write("src/main/resources/HelloWorld.drl",
-				ResourceFactory.newFileResource(rule));
-
-		// kbuilder.add( ResourceFactory.newClassPathResource(
-		// "/hellodrools/testrules.drl",HelloDroolsNew.class),ResourceType.DRL
-		// );
-
-		KieBuilder kieBuilder = kieServices.newKieBuilder(kfs);
-
-		// newKieBuilder(kfs).buildAll();
-		Results results = kieBuilder.getResults();
-		if (results.hasMessages(org.kie.api.builder.Message.Level.ERROR)) {
-			System.out.println(results.getMessages());
-			throw new IllegalStateException("### errors ###");
-		}
-
-		KieContainer kieContainer = kieServices.newKieContainer(kieServices
-				.getRepository().getDefaultReleaseId());
-		KieBase kieBase = kieContainer.getKieBase();
-
-		File compiledRuleFile = new File(getClass().getClassLoader()
-				.getResource("compiledRules").getPath());
-		// File newTemporaryFile = Files.newTemporaryFile();
-		System.out.println(compiledRuleFile.getCanonicalPath());
-		// Serialization.serialization(kieBase, compiledRuleFile);
-		DroolsStreamUtils.streamOut(new FileOutputStream(compiledRuleFile),
-				kieBase, false);
-
-		KieBase newKieBase = (KieBase) DroolsStreamUtils.streamIn(
-				new FileInputStream(compiledRuleFile), this.getClass()
-						.getClassLoader(), false);
-		// KieBase newKieBase = Serialization.deserialization(KieBase.class, compiledRuleFile);
-
-		// (KieBase) DroolsStreamUtils.streamIn(
-		// new FileInputStream(newTemporaryFile), this.getClass()
-		// .getClassLoader(), true);
-		KieSession newKieSession = newKieBase.newKieSession();
-		newKieSession.fireAllRules();
-		//
-		// KieContainer newKieContainer =
-		// kieServices.newKieClasspathContainer();
-		// newKieContainer.newStatelessKieSession();
-		//
-		// KieSession kieSession = kieContainer.newKieSession();
-
+		    log.debug(compiledRuleFile.getCanonicalPath());
+		   
+		    Serialization.serialize(kieBase, compiledRuleFile);
+		    kieBase = Serialization.deserialize(KieBase.class, compiledRuleFile);
+		    
+		    //      KieSession kieSession = kieContainer.newKieSession();
+		    KieSession kieSession = kieBase.newKieSession();
 	}
 }
